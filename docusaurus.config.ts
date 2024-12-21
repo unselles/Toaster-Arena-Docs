4d0b5f4b82edd9932c32155526fd4ec1c8e60ab1
unselles <pumape2@seznam.cz>
2024-12-21 03:32:51 +0100

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://toaster-arena-docs.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/<Toaster-Arena-Docs>/',
+  baseUrl: '/Toaster-Arena-Docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
