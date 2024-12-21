b13b07715fd95d50f93c09aa0e33fd16c2912117
unselles <pumape2@seznam.cz>
2024-12-21 03:48:51 +0100

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,19 +6,19 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: 'Toaster Arena Docs',
-  tagline: 'Official Documentation',
+  tagline: '',
   favicon: 'img/T_PlaceholderLogo.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://toaster-arena-docs.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Toaster Arena', // Usually your GitHub org/user name.
+  projectName: 'Toaster-Arena-Docs', // Usually your repo name.
 
   onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
@@ -66,6 +66,11 @@ const config: Config = {
   ],
 
   themeConfig: {
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+      },
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
